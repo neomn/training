1276ea8328438e1ca307041a22d095f090351790
Neo <neomn110@gmail.com>
2022-12-06 08:27:11 +0330

    php: sort given values

--- leetcode/Arrays_And_Hashing/twoSum/Solution.php
+++ leetcode/Arrays_And_Hashing/twoSum/Solution.php
@@ -1,4 +1,4 @@
-
+<?php
 class Solution {
 
     /**
@@ -7,6 +7,16 @@ class Solution {
     * @return Integer[]
     */
     function twoSum($nums, $target) {
+
+        sort($nums);
+        return $nums;
         
     }
 }
+
+$solution = new Solution;
+$nums = [15,33,13,35,10,40,5,1,88,54,18,25,22];
+$target = 47;
+var_dump( $solution->twoSum($nums, $target));
+echo 'done';
+
