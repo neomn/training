ecb2f0314ed2edccb7363684564a03763dedb05e
Neo <neomn110@gmail.com>
2022-12-06 07:30:10 +0330

    php: add php tag

---
+++ leetcode/Arrays_And_Hashing/twoSum/Solution.php
@@ -0,0 +1,12 @@
+
+class Solution {
+
+    /**
+    * @param Integer[] $nums
+    * @param Integer $target
+    * @return Integer[]
+    */
+    function twoSum($nums, $target) {
+        
+    }
+}
