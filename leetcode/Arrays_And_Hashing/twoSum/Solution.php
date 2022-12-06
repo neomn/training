34e6419f20f255f9d431490c684c56f97df44067
Neo <neomn110@gmail.com>
2022-12-06 09:07:37 +0330

    php: twoSum-> merge removeAndSort function into solution class

--- leetcode/Arrays_And_Hashing/twoSum/Solution.php
+++ leetcode/Arrays_And_Hashing/twoSum/Solution.php
@@ -2,6 +2,18 @@
 
 class Solution {
 
+
+    // remove elements bigger than target , sort the rest elements
+    function removeAndSort($nums, $target){
+
+        foreach($nums as $index=>$num){
+            if ($num > $target)
+                unset($nums[$index]);
+        }
+        sort($nums);
+        return $nums;
+    }
+
     /**
     * @param Integer[] $nums
     * @param Integer $target
@@ -17,21 +29,6 @@ class Solution {
     $nums = [15,33,13,35,10,40,5,1,88,54,18,25,22];
     $target = 47;
 
-    // remove elements bigger than target , sort the rest elements
-    function removeAndSort($nums, $target){
-
-        foreach($nums as $index=>$num){
-            if ($num > $target)
-                unset($nums[$index]);
-        }
-        sort($nums);
-        return $nums;
-    }
-
-
-    $nums = removeAndSort($nums, $target);
-    var_dump($nums);
-
-    //$solution = new Solution;
-    //var_dump( $solution->twoSum($nums, $target));
+    $solution = new Solution;
+    var_dump( $solution->twoSum($nums, $target));
 
