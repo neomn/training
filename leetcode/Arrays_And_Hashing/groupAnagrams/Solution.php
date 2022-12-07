ed28963a470a8b0a8163aeeb5c64a5f32cb8bcab
neomn110 <neomn110@gmail.com>
2022-12-07 17:54:33 +0330

    php: groupAnagrams-> add comment to how to solve the problem

--- leetcode/Arrays_And_Hashing/groupAnagrams/Solution.php
+++ leetcode/Arrays_And_Hashing/groupAnagrams/Solution.php
@@ -5,9 +5,17 @@ class Solution
     function groupAnagrams($strs)
     {
         $groupedAnagrams = [];
-        foreach ($strs as $str) {
 
+        foreach ($strs as $index=>$str) {
+            //get first element of strs
+            // count chars
+            // add key (counted chars) and value (str) to hashmap
+            // unset str
+            // iterate throw all remaning element of strs and if it is an anagram for previous str ,
+                // then add it ro str array
+            // unset matched str
         }
+
         return $groupedAnagrams;
     }
 }
