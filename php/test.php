b944857cf40d482d92f037b4d1f396959d7bfb9c
neomn110 <neomn110@gmail.com>
2023-02-17 10:17:37 +0330

    php point: some points about floats

--- php/test.php
+++ php/test.php
@@ -2,6 +2,11 @@
 
 # some points about floats
 
-echo floor(5);
+echo floor(5.4) . "\n";
+echo ceil(5.4) . "\n";
+
+echo ceil((0.1 + 0.2) * 10) . "\n"; // result=3 might be expected but it is 4
+
+// never compare floats like float x == float y 
 
 
