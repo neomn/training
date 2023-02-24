48124c015db8c395faac50d88f8c38a74bf4bd9f
neomn110 <neomn110@gmail.com>
2023-02-24 17:34:31 +0330

    python-official-docs: floor division

--- python/officalPythonDocs/1- An Informal Introduction to Python.py
+++ python/officalPythonDocs/1- An Informal Introduction to Python.py
@@ -1,9 +1,19 @@
-2 + 2
+>>>2 + 2
 4
-50 - 5*6
+>>>50 - 5*6
 20
-(50 - 5*6) / 4
+>>>(50 - 5*6) / 4
 5.0
-8 / 5  # division always returns a floating point number
+>>>8 / 5  # division always returns a floating point number
 1.6
 
+
+#floor division >>
+>>>17 / 3  # classic division returns a float
+5.666666666666667
+>>>17 // 3  # floor division discards the fractional part
+5
+>>>17 % 3  # the % operator returns the remainder of the division
+2
+>>>5 * 3 + 2  # floored quotient * divisor + remainder
+17
