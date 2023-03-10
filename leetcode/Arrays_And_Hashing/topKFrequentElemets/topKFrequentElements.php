7a950ffa00b81a6f4b119208391fa34b5e2871c4
neomn110 <neomn110@gmail.com>
2023-03-10 21:45:56 +0330

    refactor: add leetcode_solutoins to the gitignore

--- leetcode/Arrays_And_Hashing/topKFrequentElemets/topKFrequentElements.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-function topKMostFrequentElements($inputArray, $K){
-    $inputArrayLength = count($inputArray);
-    $result = array();
-    foreach($inputArray as $key => $item){
-        echo $item;
-    }
-}
